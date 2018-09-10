1359c26dcf65509cb0b3524bfa3887b8640c272a
Richard G <richard.glenski@gmail.com>
2018-09-09 21:24:08 -0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -28,4 +28,18 @@ var app4 = new Vue({
     ]
   }
 })
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+    doThings: function () {
+      this.message = 'Hey there, its Done'
+    }
+  }
+})
 
